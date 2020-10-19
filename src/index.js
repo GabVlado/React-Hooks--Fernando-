397ea14b9fb199f6b63e37a6263da69a6ead039f
Gabo Vlad <gv.valdivieso2309@gmail.com>
2020-10-19 13:31:26 -0500

    useCounter hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HooksApp } from './HooksApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+//import { CounterApp } from './components/01-useState/CounterApp';
+//import { HooksApp } from './HooksApp';
 
 
 ReactDOM.render(
 
-    <HooksApp/>, 
+    <CounterWithCustomHook/>, 
   document.getElementById('root')
 );
