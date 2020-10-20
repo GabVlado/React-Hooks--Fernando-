7349fe4126bae32e3522401cb81f4c81a07265ba
Gabo Vlad <gv.valdivieso2309@gmail.com>
2020-10-20 16:43:40 -0500

    useMemo memoriza cuando debe ejecutar una funcion dependiendo de algo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Memorize } from './components/06-memos/Memorize';
+import { MemoHook } from './components/06-memos/MemoHook';
+//import { Memorize } from './components/06-memos/Memorize';
 //import { Layout } from './components/05-useLayoutEffect/Layout';
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
@@ -14,6 +15,6 @@ import { Memorize } from './components/06-memos/Memorize';
 
 ReactDOM.render(
 
-    < Memorize />, 
+    < MemoHook />, 
   document.getElementById('root')
 );
